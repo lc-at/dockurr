ae72165b107d3e30c16b135bfa5de3c8260e4394
Faiz Jazadi <me@lcat.dev>
2023-05-22 08:28:39 +0700

    feat: Add stop_container method in containerman.py
    Signed-off-by: Faiz Jazadi <me@lcat.dev>

--- dockurr/tasks/__init__.py
+++ dockurr/tasks/__init__.py
@@ -1,5 +1,13 @@
+from celery.schedules import crontab
+
 config = {
     # overriden by config.toml
     'include': ['dockurr.tasks.containerman',
-                'dockurr.tasks.container_scheduler']
+                'dockurr.tasks.container_scheduler'],
+    'beat_schedule': {
+        'container-reaper': {
+            'task': 'dockurr.tasks.containerman.container_reaper',
+            'schedule': crontab(minute='5')
+        }
+    }
 }
--- dockurr/tasks/containerman.py
+++ dockurr/tasks/containerman.py
@@ -3,7 +3,9 @@ import logging
 import docker
 import docker.errors
 import docker.models
+
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
 from dockurr.models import (
     db,
@@ -39,7 +41,7 @@ def start_container(id_):
                 ports={f'{container.container_port}/tcp': container.public_port},
                 detach=True)
         except Exception as e:
-            logger.error('Failed to start container %s, exc: %r', id_, e)
+            logger.error('Failed to start container %s', id_, exc_info=True)
             # bye-bye, container will be reaped
             container.status = ContainerStatus.ERROR
             db.session.commit()
@@ -52,8 +54,10 @@ def start_container(id_):
     container.status = ContainerStatus.RUNNING
     db.session.add(ContainerActionLog(container.id, ContainerAction.START))
     db.session.commit()
+    logger.info('Container id=%d started', id_)
 
 
+@shared_task(ignore_result=True)
 def stop_container(id_):
     container = Container.query.filter_by(id=id_).first()
     logger.info('Stopping container id=%d', id_)
@@ -62,12 +66,14 @@ def stop_container(id_):
         raise ValueError(f'Container id={id_} not found')
     elif not container.internal_id \
             or not docker_container_exists(container.internal_id):
-        # TODO
-        pass
+        msg = f'Container {id_} status is not in sync'
+        logger.critical(msg)
+        raise ValueError(msg)
 
     if ContainerStatus(container.status) not in [
             ContainerStatus.RUNNING,
             ContainerStatus.PAUSED]:
+        logger.warning('Stopping a non-running container %s', id_)
         raise ValueError(f'Container {id_} is not stoppable')
     else:
         docker_container = client.containers.get(container.internal_id)
@@ -76,6 +82,24 @@ def stop_container(id_):
     container.status = ContainerStatus.EXITED
     db.session.add(ContainerActionLog(container.id, ContainerAction.STOP))
     db.session.commit()
+    logger.info('Container id=%d stopped', id_)
+
+
+@shared_task(ignore_result=True)
+def delete_container(id_):
+    container = Container.query.filter_by(id=id_).first()
+    logger.info('Deleting container id=%d', id_)
+
+    if not container:
+        raise ValueError(f'Container id={id_} not found')
+
+    if container.internal_id and docker_container_exists(container.internal_id):
+        docker_container = client.containers.get(container.internal_id)
+        docker_container.remove(force=True)
+
+    db.session.delete(container)
+    db.session.commit()
+    logger.info('Container id=%d deleted', id_)
 
 
 @shared_task(ignore_result=True)
@@ -84,7 +108,13 @@ def container_reaper():
     Remove a container from database, if they don't have internal ID and their
     status is ContainerStatus.ERROR
     """
-    return
+    containers = Container.query.filter_by(status=ContainerStatus.ERROR).all()
+    for container in containers:
+        logger.warning('Removing container id=%d', container.id)
+        if not container.internal_id:
+            db.session.delete(container)
+    db.session.commit()
+    logger.info('Removed %d containers', len(containers))
 
 
 @shared_task(ignore_result=True)
@@ -92,4 +122,7 @@ def container_syncronizer():
     """
     Syncronize container status on database with the real Docker container status
     """
-    return
+    containers = Container.query.filter(
+        Container.internal_id is not None).all()
+    for container in containers:
+        pass
