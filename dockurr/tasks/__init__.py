9f1347cac8aec7ee174e0d21f4078a2d8672adfd
Rachel Naragifta <rachel.naragifta@mail.ugm.ac.id>
2023-05-24 23:40:50 +0700

    feat: update container scheduler

--- dockurr/tasks/__init__.py
+++ dockurr/tasks/__init__.py
@@ -1,3 +1,4 @@
+import redis
 from dockurr.config import gconfig
 from celery.schedules import crontab
 
@@ -9,6 +10,13 @@ config = {
         'container-reaper': {
             'task': 'dockurr.tasks.containerman.container_reaper',
             'schedule': gconfig['celery']['intervals']['container_reaper'],
+        },
+        'update-beat-schedule': {
+            'task': 'dockurr.tasks.container_scheduler.update_beat_schedule',
+            'schedule': gconfig['celery']['intervals']['update_beat_schedule']
         }
+
     }
 }
+
+redbeat_rc = redis.Redis.from_url(gconfig['celery']['redbeat_redis_url'])
